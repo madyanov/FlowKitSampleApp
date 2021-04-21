9eec94e5d9d8e515bea1d8e6ed55cdfb12e4918c
Roman Madyanov <romantaken@gmail.com>
2021-04-22 01:12:10 +0300

    Remove unused code

--- FlowKitSampleApp/Sources/TransferFlowFeature/Internal/Route.swift
+++ FlowKitSampleApp/Sources/TransferFlowFeature/Internal/Route.swift
@@ -3,27 +3,4 @@ enum Route {
     case confirmation(country: Country, amount: Int, completion: () -> Void)
     case success(transfer: Transfer, completion: () -> Void)
     case invalidAmount
-
-    // don't look
-    func overriding<Output>(_ overriden: @escaping (Output) -> Void) -> Route {
-        switch self {
-        case .amount(let country, let completion):
-            return .amount(country: country) {
-                completion($0)
-                ($0 as? Output).map(overriden)
-            }
-        case .confirmation(let country, let amount, let completion):
-            return .confirmation(country: country, amount: amount) {
-                completion()
-                (() as? Output).map(overriden)
-            }
-        case .success(let transfer, let completion):
-            return .success(transfer: transfer) {
-                completion()
-                (() as? Output).map(overriden)
-            }
-        case .invalidAmount:
-            return .invalidAmount
-        }
-    }
 }
