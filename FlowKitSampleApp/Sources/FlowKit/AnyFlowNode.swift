b06ccf8e0cf9e7c2d67d5934c8ad9b1fdf9856cc
Roman Madyanov <romantaken@gmail.com>
2021-05-06 14:01:58 +0300

    Add missing AnyFlowNode

---
+++ FlowKitSampleApp/Sources/FlowKit/AnyFlowNode.swift
@@ -0,0 +1,36 @@
+public struct AnyFlowNode<Input, Output> {
+    private let box: AbstractFlowNodeBox<Input, Output>
+
+    init<Node: FlowNode>(node: Node) where Node.Input == Input, Node.Output == Output {
+        if let erased = node as? AnyFlowNode<Input, Output> {
+            box = erased.box
+        } else {
+            box = FlowNodeBox(node: node)
+        }
+    }
+}
+
+extension AnyFlowNode: FlowNode {
+    public func action(with input: Input) -> FlowAction<Output> {
+        return box.action(with: input)
+    }
+}
+
+// Type-erasure using virtual dispatching
+private class AbstractFlowNodeBox<Input, Output>: FlowNode {
+    func action(with: Input) -> FlowAction<Output> {
+        fatalError("Calling an abstract method with no implementation")
+    }
+}
+
+private final class FlowNodeBox<Node: FlowNode>: AbstractFlowNodeBox<Node.Input, Node.Output> {
+    private let node: Node
+
+    init(node: Node) {
+        self.node = node
+    }
+
+    override func action(with input: Input) -> FlowAction<Output> {
+        return node.action(with: input)
+    }
+}
