489149f58dd2dbf692258ef88ed91877f6a22315
Roman Madyanov <romantaken@gmail.com>
2021-05-08 02:59:43 +0300

    Add void initialize func

--- FlowKitSampleApp/Sources/FlowKit/Initialize.swift
+++ FlowKitSampleApp/Sources/FlowKit/Initialize.swift
@@ -1,3 +1,7 @@
 public func initialize<Output>(with output: Output) -> FlowAction<Output> {
     return FlowAction { $0(.success(output)) }
 }
+
+public func initialize() -> FlowAction<Void> {
+    return FlowAction { $0(.success(())) }
+}
